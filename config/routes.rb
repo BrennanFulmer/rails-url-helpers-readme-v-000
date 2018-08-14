4c146b34b7f3edcda59ad23a083f40cb1f2b9d4e
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-14 03:14:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   resources :posts, only: [:index, :show]
   
-  get '/register', to: 'users#new', as: 'register' # accessible with register_path
+  get '/register', to: 'users#new', as: 'register' # written as register_path elsewhere
   
 end
